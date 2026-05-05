2d51540f22c3ee78daf674d71c83a4fdd027e3af
kanta1025 <infrastructurekanta@gmail.com>
2026-05-05 12:09:41 +0900

    isMatch function  implementation

--- 03-string-search-in-scala/01-simple-string-search/SimpleSearch.scala
+++ 03-string-search-in-scala/01-simple-string-search/SimpleSearch.scala
@@ -1,6 +1,22 @@
 @main def simpleSearch =
   val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
   val pattern = "ドワンゴ".toSeq
-  val matchIndexes = Seq()
+
+  def isMatch(textPart: Seq[Char], pattern: Seq[Char]): Boolean =
+    var matched = true
+    for i <- 0 to pattern.length - 1 do
+      if textPart.length < pattern.length || textPart(i) != pattern(i) then
+        matched = false
+    matched
+
+  def search(text: Seq[Char], pattern: Seq[Char]): Seq[Int] =
+    var matchIndexes = Seq[Int]()
+    for i <- 0 to text.length - 1 do
+      val partText = text.slice(i, i + pattern.length)
+      if isMatch(partText, pattern) then
+        matchIndexes = matchIndexes :+ i
+    matchIndexes
+
+  val matchIndexes = search(text, pattern)
 
   println(s"出現場所: ${matchIndexes}")
